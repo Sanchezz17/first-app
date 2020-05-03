ea3ac4265ee618421a3ef565841f2e0790ad5466
TVIGes <khdv66@gmail.com>
2020-05-03 16:59:43 +0500

    Задача 10 "Врачи"

---
+++ src/ClientApp/src/utils/IEnumerableExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace covidSim.ClientApp.src.utils
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<(T, T)> Multiply<T>(this IEnumerable<T> enumerable, IEnumerable<T> other)
+        {
+            return
+                from first in enumerable
+                from second in other
+                select (first, second);
+        }
+    }
+}
