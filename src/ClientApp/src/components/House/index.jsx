40ccae4a0f8aac3e44a9cc619b1a0c4e0a4e676b
Sergey <hariton27sy@gmail.com>
2020-05-03 15:59:46 +0500

    Adding rendering shops in front

--- src/ClientApp/src/components/House/index.jsx
+++ src/ClientApp/src/components/House/index.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import styles from './styles.module.css'
 import { HOUSE_HEIGHT, HOUSE_WIDTH, MAX_HEIGHT, MAX_WIDTH } from "../../consts/sizes";
 
-export default function House({ x, y }) {
+export default function House({ x, y, isShop }) {
     const left = x / MAX_WIDTH * 100;
     const top = y / MAX_HEIGHT * 100;
     const width = HOUSE_WIDTH / MAX_WIDTH * 100;
     const height = HOUSE_HEIGHT / MAX_HEIGHT * 100;
     return (
         <div
-            className={ styles.root }
+            className={`${styles.root} ${isShop && styles.shop}`}
             style={{ left: `${ left }%`, top: `${ top }%`, width: `${width}%`, height: `${height}%` }}
         />
     );
